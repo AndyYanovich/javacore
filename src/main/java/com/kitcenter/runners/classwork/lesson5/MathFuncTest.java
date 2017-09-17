911a1596a101a23747b236d9f0305e9b87a3555b
Andy Yanovich <andy.janovich@gmail.com>
2017-09-17 12:13:36 +0300

    Lesson5. Classwork. Unit testing. Fix multiplyTest()

--- src/main/java/com/kitcenter/runners/classwork/lesson5/MathFuncTest.java
+++ src/main/java/com/kitcenter/runners/classwork/lesson5/MathFuncTest.java
@@ -1,17 +1,16 @@
 package com.kitcenter.runners.classwork.lesson5;
 
 import com.kitcenter.app.classwork.lesson5.MathFunc;
-import junit.framework.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathFuncTest {
     @Test // JUnit framework annotation
     public void multiplyTest() {
-        int a = 1, b = 2, expRes = 3;
+        int a = 1, b = 2, expRes = 2;
         int actRes = MathFunc.multiply(a, b);
         Assert.assertEquals(expRes, actRes);
-//        assertEquals(expRes, actRes);
     }
 }
